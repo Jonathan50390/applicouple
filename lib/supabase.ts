9f96dfcb57fd0d0ddb1e79d5c2e133feabd83e95
Jonathan50390 <jonathan.guillerm@gmail.com>
2025-11-16 18:55:37 +0100

    Updated package.json

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,15 @@
+import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://eueccnznvvwnmyzciwbe.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1ZWNjbnpudnZ3bm15emNpd2JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMTkwNTQsImV4cCI6MjA3ODY5NTA1NH0.yanokeP_2OFnfG5MFb8hVLMipsr_hXl_YLasnGOcqxY';
+const supabaseUrl = 'https://eueccnznvvwnmyzciwbe.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1ZWNjbnpudnZ3bm15emNpd2JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMTkwNTQsImV4cCI6MjA3ODY5NTA1NH0.yanokeP_2OFnfG5MFb8hVLMipsr_hXl_YLasnGOcqxY';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
